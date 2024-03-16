9fc4ad1d0d4a6e7d58eb414c12354fcac6da1d25
tausif104 <tausifahmed49@gmail.com>
2024-03-16 06:40:36 +0600

    homepage completed

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function ($) {
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000,
+  })
+
+  $('.testimonial-sliders').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  })
+})
