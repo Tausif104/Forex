cba6557daedb930e0afc622f28a19a250f84ca0f
tausif104 <tausifahmed49@gmail.com>
2024-03-17 06:20:59 +0600

    forex done

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,9 +25,6 @@ jQuery(document).ready(function ($) {
           slidesToScroll: 1,
         },
       },
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ],
   })
 })
